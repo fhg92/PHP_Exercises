48f75c51eb3f223c09810a3ba2d902947fe9d9d2
frankgulink <frankgulink@gmail.com>
2017-06-26 11:47:42 +0200

    commit

--- Week_4/RemainingFunctions.php
+++ Week_4/RemainingFunctions.php
@@ -162,7 +162,7 @@ $hank = array(6 => 'Hank');
 print_r(array_replace($names, $eric, $hank));
 echo '<br>'.PHP_EOL;
 
-$turkey = array(
+$norway = array(
     '1' => array(
     'country' => 'Norway',
     'continent' => 'Europe',
@@ -170,7 +170,7 @@ $turkey = array(
 ));
 // array_replace_recursive is used to replace values in a array with new values
 // recursively.
-print_r(array(array_replace_recursive($countries, $turkey)));
+print_r(array(array_replace_recursive($countries, $norway)));
 echo '<br>'.PHP_EOL;
 
 // array_reverse() returns the array with the elements in reverse order.
@@ -192,26 +192,118 @@ echo '<br>'.PHP_EOL;
 // If the third paramater is set to true, it will perform a strict type comparison.
 echo 'The index of the found value: ';
 print_r(array_search(8, $numbers, true));
+echo '<br>'.PHP_EOL;
 
-// array_splice()
+// array_splice() removes elements from an array and replaces them.
+array_splice($names, 3, 2, array('John', 'Michael'));
+print_r($names);
+echo '<br>'.PHP_EOL;
+
 // array_sum()
-// array_udiff_assoc
-// array_udiff_uassoc
-// array_udiff
-// array_uintersect_assoc
-// array_uintersect_uassoc
-// array_uintersect
-// array_unique
-// compact
-// count/sizeof
-// current 
-// each 
-// end 
-// extract
-// key
-// next
-// pos
-// prev
-// range
+$sum = [8, 10, 5];
+echo array_sum($sum).'<br>'.PHP_EOL;
+
+$a = [1 => 'a', 2 => 'b', 0 => 'c', 4 => 'd', 5 => 'g', 6 => 'h'];
+$b = [3 => 'c', 2 => 'b', 1 => 'a', 4 => 'd', 5 => 'f'];
+
+// Function keys is used for the array_udiff and array_uintersect functions.
+function keys($a, $b) {
+    if ($a !== $b) {
+        return 0;
+    }
+    return ($a <=> $b);
+}
+
+// Function values is used for the array_udiff and array_uintersect functions.
+function values($a, $b) {
+    if ($a === $b) {
+        return 0;
+    }
+    return ($a <=> $b);
+}
+
+// array_udiff_assoc() compares the arrays with additional index check and
+// returns the differences.
+// Unlike array_diff_assoc() It uses a user-defined callback function.
+print_r(array_udiff_assoc($a, $b, 'values'));
+echo '<br>'.PHP_EOL;
+
+// array_udiff_uassoc() compares the arrays with additional index check.
+// Compares data and indexes by a callback function and returns the differences.
+// Keys are used in the comparison unlike array_diff() and array_udiff().
+print_r(array_udiff_uassoc($a, $b, 'keys', 'values'));
+echo '<br>'.PHP_EOL;
+
+// array_udiff() compares the arrays by using a callback function for data
+// comparison and return the differences.
+print_r(array_udiff($a, $b, 'values'));
+echo '<br>'.PHP_EOL;
+
+// array_uintersect_assoc() compares 2 arrays by it's values, using a callback
+// function and returns the matches.
+print_r(array_uintersect_assoc($a, $b, 'values'));
+echo '<br>'.PHP_EOL;
+
+// array_uintersect_assoc() compares 2 arrays by it's keys and values, using 2 
+// callback functions and returns the matches.
+print_r(array_uintersect_uassoc($a, $b, 'keys', 'values'));
+echo '<br>'.PHP_EOL;
+
+// array_uintersect() compares 2 arrays by it's values, using a callback
+// function and returns the matches.
+print_r(array_uintersect($a, $b, 'values'));
+echo '<br>'.PHP_EOL;
+
+// array_unique() removes duplicate values from an array.
+print_r(array_unique($test));
+echo '<br>'.PHP_EOL;
+
+// compact() creates an array from variables and their values.
+$country = 'Japan';
+$continent = 'Asia';
+$language = 'Japanese';
+print_r(compact('country', 'continent', 'language'));
+echo '<br>'.PHP_EOL;
+
+// count() / sizeof(). Both functions return the amount of elements in an array.
+print_r(count($names));
+echo '<br>'.PHP_EOL;
+print_r(sizeof($names));
+echo '<br>'.PHP_EOL;
+
+// current() returns the element in an array.
+echo current($names).'<br>'.PHP_EOL;
+
+// next() returns the next element in an array.
+echo next($names).'<br>'.PHP_EOL;
+
+// pos() does the same as current().
+echo pos($names).'<br>'.PHP_EOL;
+
+// prev() returns the previous element in an array.
+echo prev($names).'<br>'.PHP_EOL;
+
+// each() returns the current element key and value and goes to the next element.
+// Deprecated as of PHP 7.2.0.
+print_r(each($names));
+echo '<br>'.PHP_EOL;
+
+// end() sets the index of an array to it's last element.
+print_r(end($names));
+echo '<br>'.PHP_EOL;
+
+// extract()
+// Don't use this on untrusted data (user input, like $_GET or $_FILES). 
+print_r(extract($names));
+echo '<br>'.PHP_EOL;
+
+// key() retrieves a key from an array.
+print_r(key($names));
+echo '<br>'.PHP_EOL;
+
+// range() creates an array with a range of elements.
+print_r(range(0, 6));
+echo '<br>'.PHP_EOL;
+print_r(range('a', 'e'));
 
 ?>
