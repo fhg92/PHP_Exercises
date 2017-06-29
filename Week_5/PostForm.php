59059ed177cf256fe7d67f9bc0fe395d9496bb54
frankgulink <frankgulink@gmail.com>
2017-06-29 16:14:56 +0200

    delete

--- Week_5/PostForm.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <body>
-        <form action='Post.php' method='post'>
-            <input type='text' name='input'/>
-            <input type='submit'/>
-        </form>
-    </body>
-</html>
