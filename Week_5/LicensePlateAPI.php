7f12e49fbdfb6671911ba69735d838efde68d80b
frankgulink <frankgulink@gmail.com>
2017-07-03 18:12:44 +0200

    RDW API Form Update

--- Week_5/LicensePlateAPI.php
+++ Week_5/LicensePlateAPI.php
@@ -1,23 +1,39 @@
 <?php
 
-$licenseplate = $_POST['kenteken'];
-if($licenseplate == null || !preg_match('/^[A-Z0-9]{6}$/',$licenseplate)) {
+$licensePlate = $_POST['licensePlate'];
+
+// If input type is not filled in or doesn't meet preg_match() requirements.
+// Redirect and show message.
+if($licensePlate == null || !preg_match('/^[A-Z0-9]{6}$/',$licensePlate)) {
     header('Location: LicensePlateForm.php?invalid'); 
 }
 
-if($licenseplate != null){
+// If input type is filled in and meets preg_match() requirements.
+if($licensePlate != null && preg_match('/^[A-Z0-9]{6}$/',$licensePlate)) {
+    
+//preg_match('/(?=.*[A-Z])(?=.*[0-9]){6}/',$licensePlate)
 $json = file_get_contents(
-    'https://opendata.rdw.nl/resource/qyrd-w56j.json?kenteken=' . $licenseplate
+    'https://opendata.rdw.nl/resource/qyrd-w56j.json?kenteken=' . $licensePlate
 );
-    
+
 $decode = json_decode($json);
-    
+
+// If license plate doesn't exist. Redirect and show message.
+if(!isset($decode[0]->kenteken)) {
+    header('Location: LicensePlateForm.php?not_registered'); 
+} 
+// If license plate exists. Show car info.    
+else {
 echo 'Kenteken: '.$decode[0]->kenteken.'<br>'.PHP_EOL.
 'Voertuigsoort: '.$decode[0]->voertuigsoort.'<br>'.PHP_EOL.
 'Merk: '.$decode[0]->merk.'<br>'.PHP_EOL.
-'Handelsbenaming: '.$decode[0]->handelsbenaming.'<br>'.PHP_EOL.
-'Bruto BPM: '.$decode[0]->bruto_bpm.'<br>'.PHP_EOL.
-'Cilinderinhoud: '.$decode[0]->cilinderinhoud.'<br>'.PHP_EOL.
+'Handelsbenaming: '.$decode[0]->handelsbenaming.'<br>'.PHP_EOL;
+if(isset($decode[0]->bruto_bpm)) {
+echo 'Bruto BPM: '.$decode[0]->bruto_bpm.'<br>'.PHP_EOL;
+} else {
+echo 'Bruto BPM: Geen informatie beschikbaar<br>'.PHP_EOL;
+}
+echo 'Cilinderinhoud: '.$decode[0]->cilinderinhoud.'<br>'.PHP_EOL.
 'Datum eerste afgifte Nederland: '.$decode[0]->datum_eerste_afgifte_nederland.
 '<br>'.PHP_EOL.
 'Datum eerste toelating: '.$decode[0]->datum_eerste_toelating.'<br>'.PHP_EOL.
@@ -27,5 +43,5 @@ echo 'Kenteken: '.$decode[0]->kenteken.'<br>'.PHP_EOL.
 $decode[0]->toegestane_maximum_massa_voertuig.'<br>'.PHP_EOL.
 'WAM verzekerd: '.$decode[0]->wam_verzekerd.'<br>'.PHP_EOL;
 }
-
+}
 ?>
--- Week_5/LicensePlateForm.php
+++ Week_5/LicensePlateForm.php
@@ -1,8 +1,8 @@
 <html>
     <body>
-        <legend>Personenauto basisdata.</legend>
+        <legend>Voer hieronder uw kenteken in zonder koppeltekens.</legend>
         <form action='LicensePlateAPI.php' method='post'>
-            <input type='text' name='kenteken' placeholder='Kenteken'/>
+            <input type='text' name='licensePlate' placeholder='1ABC23'/>
             <input type='submit'/>
         </form>
     </body>
@@ -12,7 +12,13 @@
 
 if(isset($_GET['invalid']))
 {
-echo '<span style="color:red;">Voer een geldig kenteken in.</span>';
+echo '<span style="color:red;">Voer een geldig kenteken in. 
+Het kenteken moet hoofdletters en cijfers bevatten.</span>';
+}
+
+if(isset($_GET['not_registered']))
+{
+echo '<span style="color:red;">Dit kenteken is niet geregistreerd.</span>';
 }
 
 ?>
