319fe30df9f3e27c693ba4806e0bf7218ccc1abb
frankgulink <frankgulink@gmail.com>
2017-07-26 21:19:20 +0200

    update

--- Week_7/UserFunctions.php
+++ Week_7/UserFunctions.php
@@ -39,6 +39,7 @@ function getFriendList($pdo, $curUser)
     $relations = $stmt->fetchAll();
     
     foreach($relations as $user) {
+        echo "<form method='post'>";
         if($user[0] == $curUser['user_id'] OR $user[1] == $curUser['user_id']) {
             $sql = 'SELECT username, user_id FROM user WHERE user_id = :id';
             $stmt = $pdo->prepare($sql);
@@ -50,27 +51,31 @@ function getFriendList($pdo, $curUser)
             }
             $stmt->execute();
             $friend = $stmt->fetch(PDO::FETCH_ASSOC);
-            echo '<tr><td>'.ucfirst(htmlentities($friend['username'])).' <a href = 
-            "User.php?r='. $friend['user_id'] . '"><button>Delete</button></a></td></tr>';
+            echo '<tr><td>'.ucfirst(htmlentities($friend['username'])).
+                " <button type='submit' name='delete' value='".$friend['user_id'].
+                "'>Delete</button></td></tr>";
         }
+        echo '</form>';
     }
     
-    if(isset($_GET['r']) && $_GET['r'] == $friend['user_id']) {
-        $sql= 'DELETE FROM relation WHERE user_one_id = :userOne AND user_two_id = :userTwo';
-        $stmt = $pdo->prepare($sql);
-        if($user[0] == $curUser['user_id']) {
-            $stmt->bindParam(':userOne', $curUser['user_id'], PDO::PARAM_INT);
-            $stmt->bindParam(':userTwo', $friend['user_id'], PDO::PARAM_INT);
+    if(isset($_POST['delete'])) {
+        foreach($_POST as $value) {
+            $sql= 'DELETE FROM relation WHERE user_one_id = :userOne AND user_two_id = :userTwo';
+            $stmt = $pdo->prepare($sql);
+            if($user[0] == $curUser['user_id']) {
+                $stmt->bindParam(':userOne', $curUser['user_id'], PDO::PARAM_INT);
+                $stmt->bindParam(':userTwo', $value, PDO::PARAM_INT);
+            }
+            if($user[1] == $curUser['user_id']) {
+                $stmt->bindParam(':userOne', $value, PDO::PARAM_INT);
+                $stmt->bindParam(':userTwo', $curUser['user_id'], PDO::PARAM_INT);
+            }
+            $stmt->execute();
+            header('Location: User.php');
         }
-        if($user[1] == $curUser['user_id']) {
-            $stmt->bindParam(':userOne', $friend['user_id'], PDO::PARAM_INT);
-            $stmt->bindParam(':userTwo', $curUser['user_id'], PDO::PARAM_INT);
-        }
-        $stmt->execute();
-        header('Location: User.php');
     }
     if(!isset($friend)){
-    echo "You don't have any friends in your friendlist yet.";
+        echo "You don't have any friends in your friendlist yet.";
     }
 } 
 
@@ -84,59 +89,73 @@ function getFriendRequest($pdo, $curUser)
     $stmt->bindParam(':status', $i, PDO::PARAM_INT);
     $stmt->bindParam(':id', $curUser['user_id'], PDO::PARAM_INT);
     $stmt->execute();
-    $request = $stmt->fetch(PDO::FETCH_NUM);   
-    $sql = 'SELECT username FROM user WHERE user_id = :id';
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':id', $request[0], PDO::PARAM_INT);
-    $stmt->execute();
-    $userId = $stmt->fetch(PDO::FETCH_NUM);
+    $request = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    //var_dump($request);
     
-    if(!empty($userId)) {
-        echo '<tr><td>'.ucfirst(htmlentities($userId[0])). ' has send you a friend request. '.
-            '<a href = "User.php?a=' . $request[0] . '"><button>Accept</button></a> '.
-            '<a href = "User.php?d=' .  $request[0] . '"><button>Decline</button></a></td></tr>';
-    } else {
-        echo 'There are no new friend requests.';
-    }
-    // If request accepted.
-    if(isset($_GET['a']) or isset($_GET['d'])) {
-        $sql = 'UPDATE relation SET status = :status WHERE user_two_id = :id';
+    foreach($request as $req){
+        $sql = 'SELECT username FROM user WHERE user_id = :id';
         $stmt = $pdo->prepare($sql);
-        // If friend request accepted.
-        if($_GET['a'] == $request[0]) {
-            $i = 1;
-        }
-        // If friend request declined.
-        if($_GET['d'] == $request[0]) {
-            $i = 2;
-        }
-        $stmt->bindParam(':status', $i, PDO::PARAM_INT);
-        $stmt->bindParam(':id', $curUser['user_id'], PDO::PARAM_INT);
+        $stmt->bindParam(':id', $req['user_one_id'], PDO::PARAM_INT);
         $stmt->execute();
-        $userId = null;
-        header('Location: User.php');
+        $request = $stmt->fetch();
+        
+        if(!empty($request)) {
+            echo '<tr><td>'.ucfirst(htmlentities($request[0])). ' has send you a friend request'.
+            " <button type='submit' name='accept' value='".$req['user_one_id']."'>Accept</button>
+            <button type='submit' name='decline' value='".$req['user_one_id']."'>Decline</button>
+            </td></tr>";
+        }
+        // ACCEPTING/DECLINING NOT WORKING YET. UNDER CONSTRUCTION.
+        if(isset($_POST['accept']) or isset($_POST['decline'])) {
+            $sql = 'UPDATE relation SET status = :status WHERE user_one_id = :id1
+            AND user_two_id = :id2';
+            $stmt = $pdo->prepare($sql);
+            // If friend request accepted.
+            if($_POST['accept'] == $req['user_one_id']) {
+                $i = 1;
+            }
+            // If friend request declined.
+            if($_POST['decline'] == $req['user_one_id']) {
+            $i = 2;
+            }
+            $stmt->bindParam(':status', $i, PDO::PARAM_INT);
+            $stmt->bindParam(':id1', $req['user_one_id'], PDO::PARAM_INT);
+            $stmt->bindParam(':id2', $curUser['user_id'], PDO::PARAM_INT);
+            $stmt->execute();
+            $userId = null;
+            header('Location: User.php');
+        }
+    }
+    if(empty($request)) {
+        echo 'There are no new friend requests.';
     }
 }
 
 
 function addFriend($pdo, $curUser, $otherUsers)
 {
+    echo "<form method='post'>";
     foreach($otherUsers as $user) {
-        echo '<tr><td>'.ucfirst(htmlentities($user['username'])).' <a href = "User.php?add='
-            . $user['user_id'] . '"><button>Add</button></a></td></tr>';
-    
-        if(isset($_GET['add']) && $_GET['add'] == $user['user_id']) {
+        echo '<tr><td>'.ucfirst(htmlentities($user['username'])).
+        " <button type='submit' name='add' value='".$user['user_id']."'>Add</button>
+        </td></tr>";
+        
+        if(isset($_POST['add'])) {
+            foreach($_POST as $value) {
             // Status.
             $i = 0;
             
-            $sql = "INSERT INTO relation(user_one_id, user_two_id, status) VALUES (:curId, :otherId, :status)";
+            $sql = "INSERT INTO relation(user_one_id, user_two_id, status) VALUES
+            (:curId, :otherId, :status)";
             $stmt = $pdo->prepare($sql);
             $stmt->bindParam(':curId', $curUser['user_id'], PDO::PARAM_INT);
-            $stmt->bindParam(':otherId', $user['user_id'], PDO::PARAM_INT);
+            $stmt->bindParam(':otherId', $value, PDO::PARAM_INT);
             $stmt->bindParam(':status', $i, PDO::PARAM_INT);
             $stmt->execute();
+            }
         }
     }
+    echo '</form>';
 }
 
 ?>
