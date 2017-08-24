c2643bb7e249ad6ecfed76c44eb6325674c281b3
frankgulink <frankgulink@gmail.com>
2017-08-24 12:32:48 +0200

    commit

--- Week_8/REST/Index.php
+++ Week_8/REST/Index.php
@@ -1,5 +1,3 @@
-<!-- Not done yet. -->
-
 <?php
 
 require_once('DbConnect.php');
@@ -10,8 +8,7 @@ $data = json_decode(file_get_contents('php://input'));
 
 switch($method) {
     case 'GET':
-        $sql = 'SELECT * FROM user u INNER JOIN user_personal up ON u.user_id 
-        = up.user_id WHERE u.user_id = :id';
+        $sql = 'SELECT * FROM gender WHERE gender_id = :id';
         $stmt = $pdo->prepare($sql);
         $stmt->bindValue(':id', $_GET['json'], PDO::PARAM_INT);
         $stmt->execute();
@@ -19,31 +16,18 @@ switch($method) {
         echo json_encode($details);
         break;
     case 'POST':
-        $stmt = $pdo->prepare('INSERT INTO user(email, password) 
-        VALUES (?, ?)');
-        $stmt->execute([$data->email, $data->password]);
-            
-        $sql = 'INSERT INTO user_personal(first_name, last_name, city, 
-        date_of_birth, gender_id) VALUES (?, ?, ?, ?, ?)';
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$data->first_name, $data->last_name, $data->city,
-                      $data->date_of_birth, $data->gender]);
+        $stmt = $pdo->prepare('INSERT INTO gender(label) VALUES (?)');
+        $stmt->execute([$data->label]);
         break;
     case 'PUT':
-        $sql = 'UPDATE user_personal SET first_name = :first, last_name = :last
-        , city = :city, date_of_birth = :dob, gender_id = :gender WHERE 
-        user_id = :id';
+        $sql = 'UPDATE gender SET label = :label WHERE gender_id = :id';
         $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':label', $data->label, PDO::PARAM_STR);
         $stmt->bindValue(':id', $_GET['json'], PDO::PARAM_INT);
-        $stmt->bindValue(':first', $data->first_name, PDO::PARAM_STR);
-        $stmt->bindValue(':last', $data->last_name, PDO::PARAM_STR);
-        $stmt->bindValue(':city', $data->city, PDO::PARAM_STR);
-        $stmt->bindValue(':dob', $data->date_of_birth, PDO::PARAM_STR);
-        $stmt->bindValue(':gender', $data->gender_id, PDO::PARAM_INT);
         $stmt->execute();
         break;
     case 'DELETE':
-        $sql = 'DELETE FROM user WHERE user_id = :id';
+        $sql = 'DELETE FROM gender WHERE gender_id = :id';
         $stmt = $pdo->prepare($sql);
         $stmt->bindValue(':id', $_GET['json'], PDO::PARAM_INT);
         $stmt->execute();
